6ada3a72d1af1cb1cb09ca5f84fdca165ed20f45
andycarrion <andresjncarrion@gmail.com>
2022-09-14 21:21:46 -0300

    Ejercicio 1

---
+++ funciones.py
@@ -0,0 +1,25 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      HP
+#
+# Created:     14/09/2022
+# Copyright:   (c) HP 2022
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+## EJERCICIO 1 /////////////////////////////////////////////////
+
+def area_rectangulo(base, altura):
+    area_rec = base * altura
+    return area_rec
+
+area_rectangulo(7, 10)
+
+
+if __name__ == '__main__':
+    main()
